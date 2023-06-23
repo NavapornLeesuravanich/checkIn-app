26e80c7f8d52daf4e2d022c245a8d08f6a651428
hwaky <hwak001@gmail.com>
2023-06-23 16:44:17 +0700

    first commit
    firebase + crud

---
+++ lib/page/pageTwo.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page 2'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Go Back'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
